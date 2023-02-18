61b0990211c407df34be1c795c596460890afa0a
IgorErin <erin.igore@yandex.ru>
2023-02-18 23:24:52 +0300

    refactor: net6 -> net7, local tools update

--- tests/MiniScaffold.Tests/Main.fs
+++ tests/MiniScaffold.Tests/Main.fs
@@ -1,12 +1,8 @@
 namespace MiniScaffold.Tests
 
 module Main =
-    open Infrastructure
-    open System
-    open Fake.Core
     open Expecto
-    open Expecto.Logging
 
     [<EntryPoint>]
     let main argv =
-        Tests.runTestsInAssembly defaultConfig argv
+        runTestsInAssembly defaultConfig argv
